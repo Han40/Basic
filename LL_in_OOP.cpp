8038dbb8786931de382a5e61d524d0a01b3846eb
Pupil <daction-z@hotmail.com>
2015-03-31 13:45:50 -0700

    LL
    OOP

---
+++ LL_in_OOP.cpp
@@ -0,0 +1,98 @@
+// Singly Linked List in OOP techniques
+// This code is convertible to Doubly Linked List
+// Mar31
+// Base on credit:
+// http://www.cprogramming.com/snippets/source-code/double-linked-list-cplusplus
+
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    double value; // Data
+    //Node* next;
+    Node *N, *P;
+    Node(double y)
+    {
+        value = y;
+        N = P = NULL;
+    }
+};
+
+class doubleLinkedList{
+    Node* front;
+    Node* back;
+
+public:
+    doubleLinkedList()
+    {
+        front = NULL;
+        back = NULL;
+    }
+    ~doubleLinkedList(){
+        destroyList();
+    }
+    void appendNodeFront (double x);
+    void appendNodeBack (double x);
+
+    void dispNodesForward();
+    void dispNodesReverse();
+    void destroyList();
+};
+
+void doubleLinkedList::appendNodeFront(double x)
+{
+    Node* n = new Node(x);
+    if( front == NULL)
+    {
+        front = n;
+        back = n;
+    }
+    else{
+            front->P = n;
+            n->N = front;
+            front = n;
+    }
+}
+
+void doubleLinkedList::dispNodesForward(){
+    Node *temp = front;
+    cout << "\n\n Nodes in forward order:" <<endl;
+    while(temp != NULL){
+        cout << temp->value << " ";
+        temp = temp->N;
+    }
+
+}
+
+void doubleLinkedList::appendNodeBack(double x){
+
+}
+
+void doubleLinkedList::destroyList()
+{
+    Node* T = back;
+    while(T != NULL){
+        Node* T2 = T;
+        T = T->P;
+        delete T2;
+    }
+    front = NULL;
+    back  = NULL;
+}
+
+int main(){
+
+    doubleLinkedList* list = new doubleLinkedList();
+    for( int i = 1; i < 4; i++ ){
+        list->appendNodeFront(i*1.1);
+        list->dispNodesForward();
+       
+    }
+	
+    delete list;
+    return 0;
+
+
+
+}
